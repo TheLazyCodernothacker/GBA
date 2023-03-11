805f198773a40070fb51b968b033d6105b995c53
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2023-03-10 18:06:55 -0800

    Update IodineGBACoreGlueCode.js

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -2,7 +2,6 @@
 
 var games = {
     "007":"007 - Everything Or Nothing",
-    "Madden NFL 07 (USA)" : "Madden 07"
     "advancewars":"Advance Wars",
     "advancewars2":"Advance Wars 2",
     "aladdin":"Aladdin",
